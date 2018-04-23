89eceeb3be6ca2af6daacf3b30a37691b739be97
Colin Reeder <vpzomtrrfrt@gmail.com>
2018-04-22 22:18:07 -0600

    Fix startup crash, use https for forge maven

--- src/main/kotlin/click/vpzom/mods/japta2/block/FluxHopper.kt
+++ src/main/kotlin/click/vpzom/mods/japta2/block/FluxHopper.kt
@@ -5,13 +5,14 @@ import net.minecraft.block.BlockContainer
 import net.minecraft.block.material.Material
 import net.minecraft.block.properties.PropertyEnum
 import net.minecraft.block.state.BlockStateContainer
+import net.minecraft.block.state.IBlockState
 import net.minecraft.tileentity.TileEntity
 import net.minecraft.util.EnumFacing
 import net.minecraft.world.World
 
-object BlockFluxHopper: BlockContainer(Material.IRON) {
-	final val FACING: PropertyEnum<EnumFacing> = PropertyEnum.create("facing", EnumFacing::class.java)
+val FACING = PropertyEnum.create("facing", EnumFacing::class.java)
 
+object BlockFluxHopper: BlockContainer(Material.IRON) {
 	init {
 		setRegistryName("fluxhopper")
 		setUnlocalizedName("fluxhopper")
@@ -26,9 +27,15 @@ object BlockFluxHopper: BlockContainer(Material.IRON) {
 	}
 
 	override fun createBlockState(): BlockStateContainer {
-		println(FACING)
-		println(PropertyEnum.create("facing2", EnumFacing::class.java))
 		return BlockStateContainer(this, FACING)
+	}
+
+	override fun getMetaFromState(state: IBlockState): Int {
+		return state.getValue(FACING).getIndex()
+	}
+
+	override fun getStateFromMeta(meta: Int): IBlockState {
+		return getDefaultState().withProperty(FACING, EnumFacing.getFront(meta))
 	}
 }
 
