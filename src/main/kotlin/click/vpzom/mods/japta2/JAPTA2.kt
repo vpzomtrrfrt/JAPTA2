03c94ce4ad5964e82e7bde77266b81fa2339874c
Colin Reeder <vpzomtrrfrt@gmail.com>
2018-04-22 18:47:31 -0600

    Initial commit

---
+++ src/main/kotlin/click/vpzom/mods/japta2/JAPTA2.kt
@@ -0,0 +1,8 @@
+package click.vpzom.mods.japta2
+
+import net.minecraftforge.fml.common.Mod
+
+@Mod(modid = JAPTA2.ID, name = "JAPTA2", modLanguageAdapter = "net.shadowfacts.forgelin.KotlinAdapter")
+object JAPTA2 {
+	const val ID = "japta2"
+}
