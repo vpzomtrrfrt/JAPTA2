8efb83aa5cec2b78851f2023aa3d5ff9a184a17e
Colin Reeder <vpzomtrrfrt@gmail.com>
2018-06-13 23:00:49 -0600

    More TESLA compat

--- src/main/kotlin/click/vpzom/mods/japta2/block/util/EnergyHelper.kt
+++ src/main/kotlin/click/vpzom/mods/japta2/block/util/EnergyHelper.kt
@@ -4,12 +4,14 @@ import click.vpzom.mods.japta2.block.util.TileEntityJPTBase
 import click.vpzom.mods.japta2.item.util.ItemJPT
 import net.darkhax.tesla.api.ITeslaConsumer
 import net.darkhax.tesla.api.ITeslaHolder
+import net.darkhax.tesla.api.ITeslaProducer
 import net.minecraft.item.ItemStack
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 import net.minecraftforge.common.capabilities.Capability
 import net.minecraftforge.common.capabilities.CapabilityInject
+import net.minecraftforge.common.capabilities.ICapabilityProvider
 import net.minecraftforge.energy.IEnergyStorage
 
 object EnergyHelper {
@@ -17,9 +19,11 @@ object EnergyHelper {
 	lateinit var CAPABILITY_FORGE_ENERGY_STORAGE: Capability<IEnergyStorage>
 
 	@CapabilityInject(ITeslaConsumer::class)
-	lateinit var CAPABILITY_TESLA_CONSUMER: Capability<ITeslaConsumer>
+	var CAPABILITY_TESLA_CONSUMER: Capability<ITeslaConsumer>? = null
 	@CapabilityInject(ITeslaHolder::class)
-	lateinit var CAPABILITY_TESLA_HOLDER: Capability<ITeslaHolder>
+	var CAPABILITY_TESLA_HOLDER: Capability<ITeslaHolder>? = null
+	@CapabilityInject(ITeslaProducer::class)
+	var CAPABILITY_TESLA_PRODUCER: Capability<ITeslaProducer>? = null
 
 	fun getStoredEnergy(world: World, pos: BlockPos, side: EnumFacing?): Pair<Long, Long>? {
 		val te = world.getTileEntity(pos)
@@ -27,17 +31,14 @@ object EnergyHelper {
 			return Pair(te.getStoredEnergy(side), te.getMaxStoredEnergy(side))
 		}
 		if(te != null) {
-			if(te.hasCapability(CAPABILITY_TESLA_HOLDER, side)) {
-				val cap = te.getCapability(CAPABILITY_TESLA_HOLDER, side)
-				if(cap != null) {
-					return Pair(cap.getStoredPower(), cap.getCapacity())
-				}
+			val capTesla = getCapability(te, CAPABILITY_TESLA_HOLDER, side)
+			if(capTesla != null) {
+				return Pair(capTesla.getStoredPower(), capTesla.getCapacity())
 			}
-			if(te.hasCapability(CAPABILITY_FORGE_ENERGY_STORAGE, side)) {
-				val cap = te.getCapability(CAPABILITY_FORGE_ENERGY_STORAGE, side)
-				if(cap != null) {
-					return Pair(cap.getEnergyStored().toLong(), cap.getMaxEnergyStored().toLong())
-				}
+
+			val capFE = getCapability(te, CAPABILITY_FORGE_ENERGY_STORAGE, side)
+			if(capFE != null) {
+				return Pair(capFE.getEnergyStored().toLong(), capFE.getMaxEnergyStored().toLong())
 			}
 		}
 		return null
@@ -49,11 +50,14 @@ object EnergyHelper {
 			return te.attemptExtractEnergy(side, maxExtract, simulate)
 		}
 		if(te != null) {
-			if(te.hasCapability(CAPABILITY_FORGE_ENERGY_STORAGE, side)) {
-				val cap = te.getCapability(CAPABILITY_FORGE_ENERGY_STORAGE, side)
-				if(cap != null) {
-					return cap.extractEnergy(Math.min(maxExtract, Int.MAX_VALUE.toLong()).toInt(), simulate).toLong()
-				}
+			val capTesla = getCapability(te, CAPABILITY_TESLA_PRODUCER, side)
+			if(capTesla != null) {
+				return capTesla.takePower(maxExtract, simulate)
+			}
+
+			val capFE = getCapability(te, CAPABILITY_FORGE_ENERGY_STORAGE, side)
+			if(capFE != null) {
+				return capFE.extractEnergy(Math.min(maxExtract, Int.MAX_VALUE.toLong()).toInt(), simulate).toLong()
 			}
 		}
 		return 0
@@ -65,11 +69,14 @@ object EnergyHelper {
 			return te.attemptInputEnergy(side, maxInsert, simulate)
 		}
 		if(te != null) {
-			if(te.hasCapability(CAPABILITY_FORGE_ENERGY_STORAGE, side)) {
-				val cap = te.getCapability(CAPABILITY_FORGE_ENERGY_STORAGE, side)
-				if(cap != null) {
-					return cap.receiveEnergy(Math.min(maxInsert, Int.MAX_VALUE.toLong()).toInt(), simulate).toLong()
-				}
+			val capTesla = getCapability(te, CAPABILITY_TESLA_CONSUMER, side)
+			if(capTesla != null) {
+				return capTesla.givePower(maxInsert, simulate)
+			}
+
+			val capFE = getCapability(te, CAPABILITY_FORGE_ENERGY_STORAGE, side)
+			if(capFE != null) {
+				return capFE.receiveEnergy(Math.min(maxInsert, Int.MAX_VALUE.toLong()).toInt(), simulate).toLong()
 			}
 		}
 		return 0
@@ -90,12 +97,24 @@ object EnergyHelper {
 			return item.receiveEnergy(stack, max, false)
 		}
 
-		if(stack.hasCapability(CAPABILITY_FORGE_ENERGY_STORAGE, null)) {
-			val cap = stack.getCapability(CAPABILITY_FORGE_ENERGY_STORAGE, null)
-			if(cap == null) return 0
-			return cap.receiveEnergy(longToInt(max), false).toLong()
+		val capTC = getCapability(stack, CAPABILITY_TESLA_CONSUMER, null)
+		if(capTC != null) {
+			return capTC.givePower(max, false)
+		}
+
+		val capFE = getCapability(stack, CAPABILITY_FORGE_ENERGY_STORAGE, null)
+		if(capFE != null) {
+			return capFE.receiveEnergy(longToInt(max), false).toLong()
 		}
 
 		return 0
+	}
+
+	fun <T> getCapability(holder: ICapabilityProvider, cap: Capability<T>?, side: EnumFacing?): T? {
+		if(cap == null) return null
+
+		if(!holder.hasCapability(cap, side)) return null
+
+		return holder.getCapability(cap, side)
 	}
 }
--- src/main/kotlin/click/vpzom/mods/japta2/block/util/TileEntityJPTBase.kt
+++ src/main/kotlin/click/vpzom/mods/japta2/block/util/TileEntityJPTBase.kt
@@ -1,6 +1,8 @@
 package click.vpzom.mods.japta2.block.util
 
 import net.darkhax.tesla.api.ITeslaConsumer
+import net.darkhax.tesla.api.ITeslaHolder
+import net.darkhax.tesla.api.ITeslaProducer
 import net.minecraft.tileentity.TileEntity
 import net.minecraft.util.EnumFacing
 import net.minecraftforge.common.capabilities.Capability
@@ -20,9 +22,20 @@ abstract class TileEntityJPTBase: TileEntity() {
 		}
 	}
 
-	inner class TeslaAdapter(val side: EnumFacing?): ITeslaConsumer {
+	inner class TeslaAdapter(val side: EnumFacing?): ITeslaConsumer, ITeslaHolder, ITeslaProducer {
 		override fun givePower(max: Long, simulate: Boolean): Long {
 			return attemptInputEnergy(side, max, simulate)
+		}
+		
+		override fun getStoredPower(): Long {
+			return getStoredEnergy(side)
+		}
+		override fun getCapacity(): Long {
+			return getMaxStoredEnergy(side)
+		}
+
+		override fun takePower(max: Long, simulate: Boolean): Long {
+			return attemptExtractEnergy(side, max, simulate)
 		}
 	}
 
@@ -33,11 +46,21 @@ abstract class TileEntityJPTBase: TileEntity() {
 
 	override fun <T> getCapability(capability: Capability<T>, side: EnumFacing?): T? {
 		if(capability == EnergyHelper.CAPABILITY_FORGE_ENERGY_STORAGE) return ForgeEnergyAdapter(side) as T
+		if(capability == EnergyHelper.CAPABILITY_TESLA_HOLDER
+		|| capability == EnergyHelper.CAPABILITY_TESLA_PRODUCER
+		|| capability == EnergyHelper.CAPABILITY_TESLA_CONSUMER) {
+			return TeslaAdapter(side) as T
+		}
 		return super.getCapability(capability, side)
 	}
 
 	override fun hasCapability(capability: Capability<*>, side: EnumFacing?): Boolean {
 		if(capability == EnergyHelper.CAPABILITY_FORGE_ENERGY_STORAGE) return true
+		if(capability == EnergyHelper.CAPABILITY_TESLA_HOLDER
+		|| capability == EnergyHelper.CAPABILITY_TESLA_PRODUCER
+		|| capability == EnergyHelper.CAPABILITY_TESLA_CONSUMER) {
+			return true
+		}
 		return super.hasCapability(capability, side)
 	}
 }
