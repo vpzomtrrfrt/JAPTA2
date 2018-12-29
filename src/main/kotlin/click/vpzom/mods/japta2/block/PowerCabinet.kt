8f05190d74cb749288c8460c2bffc93203d3f90d
Colin Reeder <vpzomtrrfrt@gmail.com>
2018-12-29 14:07:33 -0700

    power cabinet stuff?

--- src/main/kotlin/click/vpzom/mods/japta2/block/PowerCabinet.kt
+++ src/main/kotlin/click/vpzom/mods/japta2/block/PowerCabinet.kt
@@ -8,19 +8,27 @@ import net.minecraft.block.Material
 import net.minecraft.state.property.IntegerProperty
 import net.minecraft.item.Item
 import net.minecraft.item.ItemGroup
+import net.minecraft.item.ItemPlacementContext
 import net.minecraft.item.ItemStack
 import net.minecraft.item.block.BlockItem
 import net.minecraft.state.StateFactory
 import net.minecraft.util.DefaultedList
+import net.minecraft.util.Identifier
 import net.minecraft.world.World
 
 val VALUE = IntegerProperty.create("value", 0, 15)
+val KEY_VALUE = "Value"
 
 object BlockPowerCabinet: Block(Block.Settings.of(Material.METAL).strength(2f, 2f)) {
 	val LINE_VALUE = 2000
 	val PROP_VALUE = VALUE
 
 	object ItemPowerCabinet: BlockItem(BlockPowerCabinet, Item.Settings().itemGroup(ItemGroup.REDSTONE)) {
+		init {
+			addProperty(Identifier(JAPTA2.ID, "powercabinet_value")) { stack, _, _ ->
+				(stack.tag?.getByte(KEY_VALUE) ?: 0).toFloat()
+			}
+		}
 	}
 
 	override fun appendProperties(builder: StateFactory.Builder<Block, BlockState>) {
@@ -39,4 +47,10 @@ object BlockPowerCabinet: Block(Block.Settings.of(Material.METAL).strength(2f, 2
 	}
 
 	override fun getRenderLayer(): BlockRenderLayer = BlockRenderLayer.CUTOUT
+
+	override fun getPlacementState(ctx: ItemPlacementContext): BlockState {
+		val value = ctx.itemStack.tag?.getByte(KEY_VALUE) ?: 0
+
+		return defaultState.with(VALUE, value.toInt())
+	}
 }
