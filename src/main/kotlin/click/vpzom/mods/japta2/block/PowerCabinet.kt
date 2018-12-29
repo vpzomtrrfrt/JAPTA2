eafee01711a7be0a50ed8dc07f5780097ed1d7a8
Colin Reeder <vpzomtrrfrt@gmail.com>
2018-12-29 14:52:04 -0700

    Power Cabinet item stuff

--- src/main/kotlin/click/vpzom/mods/japta2/block/PowerCabinet.kt
+++ src/main/kotlin/click/vpzom/mods/japta2/block/PowerCabinet.kt
@@ -1,6 +1,8 @@
 package click.vpzom.mods.japta2.block
 
 import click.vpzom.mods.japta2.JAPTA2
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
 import net.minecraft.block.Block
 import net.minecraft.block.BlockRenderLayer
 import net.minecraft.block.BlockState
@@ -14,7 +16,10 @@ import net.minecraft.item.block.BlockItem
 import net.minecraft.state.StateFactory
 import net.minecraft.util.DefaultedList
 import net.minecraft.util.Identifier
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.BlockView
 import net.minecraft.world.World
+import net.minecraft.world.loot.context.LootContext
 
 val VALUE = IntegerProperty.create("value", 0, 15)
 val KEY_VALUE = "Value"
@@ -35,12 +40,25 @@ object BlockPowerCabinet: Block(Block.Settings.of(Material.METAL).strength(2f, 2
 		builder.with(VALUE)
 	}
 
-	// TODO fix drops
+	private fun getItemStack(state: BlockState): ItemStack {
+		val stack = ItemStack(this, 1)
+		stack.getOrCreateTag().putByte(KEY_VALUE, state.get(VALUE).toByte())
+		return stack
+	}
+
+	override fun getDroppedStacks(state: BlockState, ctxBuilder: LootContext.Builder): List<ItemStack> {
+		return listOf(getItemStack(state))
+	}
+
+	@Environment(EnvType.CLIENT)
+	override fun getPickStack(view: BlockView, pos: BlockPos, state: BlockState): ItemStack {
+		return getItemStack(state)
+	}
 
 	override fun addStacksForDisplay(tab: ItemGroup, list: DefaultedList<ItemStack>) {
 		val stackEmpty = ItemStack(this, 1)
 		val stackFull = ItemStack(this, 1)
-		stackFull.setDamage(15)
+		stackFull.getOrCreateTag().putByte(KEY_VALUE, 15)
 
 		list.add(stackEmpty)
 		list.add(stackFull)
